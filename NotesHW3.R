5b2f25d8c7e2dbf9e9e7ca82c6a3f10e1b9ea64b
collettc <col15008@byui.edu>
2018-01-29 14:08:17 -0700

    Uploading the Hard Work 3 RMD and some notes from class on 1/29/19

---
+++ NotesHW3.R
@@ -0,0 +1,38 @@
+#Assumption pg 103
+
+# R code to make things easier
+
+cars.lm <- lm(dist~speed, data = cars)
+summary(cars.lm)
+
+#Finding Standard Deviation of residuals
+sd(cars.lm$residuals)
+
+#Square root of the MSE
+summary(cars.lm)$sigma
+
+plot(cars.lm)
+
+#Semi-Studentized Residuals
+semistud <- (cars.lm$residuals) - mean(cars.lm$residuals)/summary(cars.lm)$sigma
+
+semistud
+
+stem(semistud)
+
+hist(semistud)
+
+stripchart(semistud, method = 'stack')
+
+boxplot(cars.lm$residuals)
+
+
+#sequence plot
+plot(cars.lm$residuals, type = 'b', pch = 16, cex = 0.8)
+
+par(mfrow = c(2,1))
+plot(cars.lm, which  = 1:2)
+
+#residuals verses x
+
+plot(cars.lm$residuals ~ cars$speed)
